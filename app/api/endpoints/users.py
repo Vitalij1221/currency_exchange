0a002802c214f77ce1d5d760c45a26334fd93896
Vitalij Potsepnia <potsepnia.vitalij@gmail.com>
2026-04-29 11:37:55 +0300

    Написание ручек для регистрации пользователя и для аутеификации

--- app/api/endpoints/users.py
+++ app/api/endpoints/users.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI, Depends,HTTPException
 from models.user import CreateUser
+from database import fake_bd
+from passlib.context import CryptContext
 
 app = FastAPI()
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated='auto')
 
-fake_bd = []
 
-@app.post("/register", response_model=CreateUser)
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+@app.post("/register")
 async def register_user(user: CreateUser):
-    fake_bd.append({'username':user.name, 'password':user.password})
+    existing_user = next((u for u in fake_bd if u["email"] == user.email or u["name"] == user.name), None)
+    if existing_user:
+        raise HTTPException(status_code=400, detail="User already exists")
+    hashed_password = hash_password(user.password)
     
+    fake_bd.append({'username':user.name, 'password':hashed_password})
+    return {'message': f"Пользователь {user.username} успешно добавлен."}
